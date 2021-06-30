c7609b028ae638c2816e458ddc838dbb6b989875
lakshayalmadi <lashay.almadi.la@gmail.com>
2021-06-30 23:02:28 +0530

    add-new-node

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./style.css";
 import "./index.css";
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Note from "./components/note";
@@ -8,10 +8,20 @@ import CreateArea from "./components/CreateArea";
 
 
 function App(){
+  const [notes, setNote]=useState([]);
+  
+  function addNote(newNote){
+    setNote(prevNote=> {
+      return [...prevNote, newNote];
+    })
+  }
   return(
     <div>
       <Header />
-      <CreateArea />
+      <CreateArea onAdd={addNote}/>
+      {notes.map((noteItem)=>{
+        return <Note title={noteItem.title} content={noteItem.content}/>
+      })}
       <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
